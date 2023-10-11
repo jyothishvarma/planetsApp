3a2d45e44c2bb1997cd9ab30d92b18a107a33e65
jyothishvarma <jyothishvarma123@gmail.com>
2023-10-11 12:41:06 +0000

    solution

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,19 @@
+// Write your code here
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+  const {name, imageUrl, description} = planetDetails
+
+  return (
+    <>
+      <div className="planet-item-container">
+        <img src={imageUrl} className="planet-image" alt={`planet ${name}`} />
+        <h1 className="planet-name">{name}</h1>
+        <p className="description">{description}</p>
+      </div>
+    </>
+  )
+}
+
+export default PlanetItem
